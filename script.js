a0280c557583d1468dd8463b67667841352c2af1
Jose Villa <josedvilla18@gmail.com>
2025-03-27 21:59:19 -0500

    Lights and Textures

--- script.js
+++ script.js
@@ -1,102 +1,156 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
+import { Wireframe } from 'three/examples/jsm/Addons.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-// TODO =========================================================
-// TODO                         SETUP
-// TODO =========================================================
+//* =========================================================
+//*                         SETUP
+//* =========================================================
 
-// ! Canvas
+//* Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// ! Scene
+//* Scene
 const scene = new THREE.Scene()
 
-// ! Sizes 
+//* Sizes 
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-window.addEventListener('resize', (event) => {
-    // Update Sizes    
-    sizes.width = window.innerWidth,
-        sizes.height = window.innerHeight
 
-    // Update Camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+//* =========================================================
+//*                         LIGHT
+//* =========================================================
 
-    // Update Renderer
-    renderer.setSize(sizes.width, sizes.height)
-})
+const ambientLight = new THREE.AmbientLight(0xffffff, .5)
+scene.add(ambientLight)
 
-// ! Cursor
-const cursor = {
-    x: 0,
-    y: 0
-}
+const pointLight = new THREE.PointLight('lightblue', 20)
+pointLight.position.set(1, 3, 1)
+scene.add(pointLight)
 
-window.addEventListener('mousemove', (event) => {
-    cursor.x = (event.clientX / sizes.width - 0.5)
-    cursor.y = - (event.clientY / sizes.height - 0.5)
-})
 
-// TODO =========================================================
-// TODO                        OBJECTS
-// TODO =========================================================
+//* =========================================================
+//*                         TEXTURE
+//* =========================================================
 
-// ! Group
-const group = new THREE.Group()
-group.position.set(0, 0, 0)
-scene.add(group)
+const textureLoader = new THREE.TextureLoader()
+const bmo = textureLoader.load('/bmo.jpg')
+const finn = textureLoader.load('/finn.jpg')
+const jake = textureLoader.load('/jake.jpg')
+const steel = textureLoader.load('/steel.jpg')
 
-// ! Cube
-const cube2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshNormalMaterial()
+const pointLightHelper = new THREE.PointLightHelper(pointLight, .2)
+
+scene.add(pointLightHelper)
+
+//* =========================================================
+//*                         OBJECT
+//* =========================================================
+
+const material = new THREE.MeshStandardMaterial()
+material.shininess = 1000
+material.metalness = .45
+material.roughness = .75
+// material.matcap = steel
+// material.opacity = 0.5|
+// material.transparent = true
+material.side = THREE.DoubleSide
+material.flatShading = true
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(.5, 100, 100), material
 )
-group.add(cube2)
 
-// ! Axes Helper
-const axesHelper = new THREE.AxesHelper(2)
-// ! scene.add(axesHelper)
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5), material
+)
+plane.position.set(2, 1, 0)
 
-// TODO =========================================================
-// TODO                         CAMERA
-// TODO =========================================================
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.1, 100, 100), material
+)
+torus.position.set(-2, 0, 0)
+scene.add(sphere, plane, torus)
+//* =========================================================
+//*                         CAMERA
+//* =========================================================
 
-// ! Camera setup
+//* Camera setup
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(1, 1, 2)
+camera.position.set(0, 0, 3)
 scene.add(camera)
 
-// ! Controls
+//* Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
-controls.update()
 
-// TODO =========================================================
-// TODO                      RENDERER
-// TODO =========================================================
+//* =========================================================
+//*                      RENDERER
+//* =========================================================
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// TODO  =========================================================
-// TODO                   ANIMATION
-// TODO  =========================================================
+// Mejorar la calidad de las texturas
+jake.magFilter = THREE.LinearFilter // o THREE.NearestFilter
+jake.minFilter = THREE.LinearMipmapLinearFilter
+jake.generateMipmaps = true
+jake.anisotropy = renderer.capabilities.getMaxAnisotropy()
+
+// También puedes aplicar lo mismo a las otras texturas
+finn.magFilter = THREE.LinearFilter
+finn.minFilter = THREE.LinearMipmapLinearFilter
+finn.generateMipmaps = true
+finn.anisotropy = renderer.capabilities.getMaxAnisotropy()
+
+bmo.magFilter = THREE.LinearFilter
+bmo.minFilter = THREE.LinearMipmapLinearFilter
+bmo.generateMipmaps = true
+bmo.anisotropy = renderer.capabilities.getMaxAnisotropy()
+
+//* =========================================================
+//*                      EVENT LISTENERS
+//* =========================================================
+
+window.addEventListener('resize', () => {
+    //* Update Sizes    
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //* Update Camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //* Update Renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+//* =========================================================
+//*                   ANIMATION
+//* =========================================================
+
+const clock = new THREE.Clock()
 
 const tick = () => {
-    // ! Update controls
+    //* Update controls
+    const elapsedTime = clock.getElapsedTime()
+    sphere.rotation.x = elapsedTime * 0.1
+    plane.rotation.x = elapsedTime * 0.1
+    torus.rotation.x = elapsedTime * 0.1
+    sphere.rotation.y = elapsedTime * 0.15
+    plane.rotation.y = elapsedTime * 0.15
+    torus.rotation.y = elapsedTime * 0.15
     controls.update()
 
-    // ! Render
+    //* Render
     renderer.render(scene, camera)
 
-    // ! Call next frame
+    //* Call next frame
     window.requestAnimationFrame(tick)
 }
 
